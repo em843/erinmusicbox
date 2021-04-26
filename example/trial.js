ef8a6fbc93cb61662cc63f4941e85dbd9cac0c63
em843 <erinmurphy843@gmail.com>
2021-04-26 19:41:49 -0400

    Get multiple notes to display
    Still troubleshooting xPosition/deltaTime dependency issue in placeNotes

--- example/trial.js
+++ example/trial.js
@@ -2,8 +2,20 @@
 Author: Erin
 Created: 4/20/21
 Modified: 4/21/21 parse events in MIDI data
+Modified: 4/26/21 display multiple shapes on canvas
 
-TODO: Find a way to determine which MIDI event is events and which is title
+WHERE YOU LEFT OFF:
+Trying to figure out a solution for placeNotes() with xPosition, xSum and deltaTime.
+I think the solution is to have two separate methods, placeNote and createNote.
+
+
+short term
+TODO:
+TODO: Figure out difference between drawCircle x y parameters and circle.x + circle.y
+TODO: Initialize grid
+TODO: Figure out proportion formulas for placing notes
+
+long term
 TODO: Add support for multiple tracks
 */
 
@@ -13,53 +25,81 @@ function onLoad(midiData) {
     // select the INPUT element that will handle
     // the file selection.
     let source = document.getElementById("filereader");
+
+    MidiParser.parse(source, function (obj) {
+        console.log(obj);
+        var stage = new createjs.Stage("demoCanvas");
+
+        // initialize note grid
+        //var shape = new createjs.Shape();
+        //shape.graphics.lineTo(10, 0);
+        
+        /*
+        for (var i = 0; i < 10; i++)
+        {
+            shape.graphics.lineTo(i*10, 0);
+            // Since shapes have no bounds, you will have to know the bounds based on what you draw:
+            shape.cache(0, 100, 50, 50);
+            var bmp = new createjs.Bitmap(shape.cacheCanvas);
+        }
+        */
+
+        // Parse notes
+        var firstNote;
+        var events = obj.track[1].event;
+
+        for (var i = 0; i < events.length; i++) {
+            console.log("Checking for the first note...");
+            if (Array.isArray(events[i].data)) { // Once we find a note, break the loop.
+                firstNote = i;
+                console.log("Found first note at index " + firstNote);
+                break;
+            }
+        }
+        console.log("Time to process events.");
+
+        for (var i = firstNote; i < events.length-1; i++) { // For each note in track chunk
+            var currEvent = events[i];
+            if (currEvent.data[1] !=0 ) { // If it's a 'note on' event, place it; otherwise ignore it
+                console.log(currEvent);
+                //console.log("Value: " + currEvent.data[0]);
+                placeNote(currEvent.data[0], currEvent[0], stage)
+            }
+        }
     
-MidiParser.parse(source, function (obj) {
-console.log(obj);
-
-// is number of elements in "data" array a good way to tell what events are notes?
-
-
-var firstNote;
-var events = obj.track[1].event;
-for (var i = 0; i < events.length; i++)
-{
-    console.log("Checking for the first note...");
-    if (Array.isArray(events[i].data)) { // Once we find a note, break the loop.
-        firstNote = i;
-        console.log("Found first note at index " + firstNote);
-        break;
+    });
     }
-}
-console.log("Time to process events.");
 
 
-for (var i = firstNote; i < events.length-1; i++)
-{
-    var currEvent = events[i];
-    if (currEvent.data[1] !=0 ) { // if it's a 'note on' event, place it; otherwise ignore it
-        console.log(currEvent);
-        placeNote(currEvent.data[0], currEvent[0])
+    function placeNote(value, deltaTime, stage){
+        var yPosition = value; // change to calculated formula once you find one that works
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, yPosition, 4); // change this to xPosition when you figure it out
+        circle.x = 100;
+        circle.y = 100;
+        stage.addChild(circle);
+        stage.update();
+        //console.log("Note should be visible on screen.");
     }
-}
-
-// initialize note grid (don't implement yet)
-// for each note in track chunk
-    // display small circle in canvas
-    // use timedelta and note value to determine position
- 
-});
-}
 
 
 
-function placeNote(value, deltaTime, stage){
-    var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 4);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
-    stage.update();
-    //console.log("Note placed.");
-}
+
+
+
+    
+
+/* Discarded code:
+
+        var xPosition;
+        var xSum;
+        if (typeof xSum === 'undefined') { // if this is the first note placed
+            xPosition = 0;
+            console.log("First note x position set.")
+        }
+        else {
+            xPosition = xSum + Math.floor(deltaTime/4);
+        } 
+        xSum = xPosition;
+
+*/
