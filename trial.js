a778109d22634223720bb14968ea8c02ce312f32
em843 <erinmurphy843@gmail.com>
2021-05-30 14:35:07 -0400

    Convert to canvas for shape drawing
    also create basic grid

--- trial.js
+++ trial.js
@@ -19,53 +19,37 @@ TODO: Add support for multiple tracks
 */
 
 
-var stage = new Rune({
-    container: "body",
-    width: 500,
-    height: 400
-});
-
-// initialize note grid
-
-
-stage.line(0, 0, 0, 294);
-stage.line(44, 0, 44, 294);
 
 
 
-var i;
-for (i = 0; i < 5; i++){
-}
-stage.draw()
+
 
 
 function onLoad(midiData) {
     console.log("load successful");
+
+    // Create canvas
+    var canvas = document.querySelector('canvas');
+    console.log(canvas)
+    canvas.width = window.innerWidth;
+    canvas.height = 500;
+    var c = canvas.getContext('2d');
+
+    // Define accepted notes (for 15 note box)
     
-    // select the INPUT element that will handle
-    // the file selection.
+
+    // Draw grid
+    gridColor = "black" // Grid line color
+    letterColor = "black" // Note letter color
+    noteColor = "#448097" // Note/hole/circle color
+    drawGrid(c, gridColor, letterColor)
+
+    // Select the INPUT element that will handle the file selection.
     let source = document.getElementById("filereader");
 
-
+    // Begin processing MIDI file
     MidiParser.parse(source, function (obj) {
-        console.log(obj);
-        
-        
-
-
-        //var shape = new createjs.Shape();
-        //shape.graphics.lineTo(10, 0);
-        
-        
-        /*
-        for (var i = 0; i < 10; i++)
-        {
-            shape.graphics.lineTo(i*10, 0);
-            // Since shapes have no bounds, you will have to know the bounds based on what you draw:
-            shape.cache(0, 100, 50, 50);
-            var bmp = new createjs.Bitmap(shape.cacheCanvas);
-        }
-        */
+        console.log("Object " + obj);
 
         // Parse notes
         var firstNote;
@@ -80,50 +64,91 @@ function onLoad(midiData) {
                 break;
             }
         }
-        console.log("Time to process events.");
 
+        // Process/place notes
+        console.log("Time to process events.");
         for (var i = firstNote; i < events.length-1; i++) { // For each note in track chunk
             var currEvent = events[i];
             var xSum = 0;
             if (currEvent.data[1] !=0 ) { // If it's a 'note on' event, place it; otherwise ignore it
-                console.log(currEvent);
-                //console.log("Value: " + currEvent.data[0]);
-                xSum += currEvent[0]; // Add deltaTime to next note's x position
-                placeNote(currEvent.data[0], currEvent[0], stage)
+                if(currEvent.data[0])
+                    console.log(currEvent);
+                    console.log("deltaTime: " + currEvent.deltaTime);
+                    xSum += currEvent.deltaTime; // Add deltaTime to next note's x position
+                    placeNote(c, xSum, currEvent.data[0], noteColor)
             }
         }
     
     });
-    }
+}
 
-
-    function placeNote(noteValue, notePlacement, stage){
-
-
-
-       
-
-
-        
-       /*
-        var yPosition = noteValue; // change to calculated formula once you find one that works
-        var xPosition = notePlacement; // change to calculated formula once you find one that works
-        var circle = new createjs.Shape();
-        circle.graphics.beginFill("DeepSkyBlue").drawCircle(100, yPosition, 4); // change this to xPosition when you figure it out
-        circle.x = 100;
-        circle.y = 100;
-        stage.addChild(circle);
-        stage.update();
-        //console.log("Note should be visible on screen.");
-        */
-    }
-
-
-
-
-
-
+function drawGrid(c, gridColor, letterColor){
     
+    var noteLetters = ["C", "D", "E", "F", "G", "A", "B",
+                    "C", "D", "E", "F", "G", "A", "B", "C"]
+
+    c.font = "18px Arial";
+    c.fillStyle = letterColor;
+    for (var i = 0; i < noteLetters.length; i+=1){
+        var y = i * 20 + 47
+        c.fillText(noteLetters[i], 20, y);
+    }
+    
+    p = 40; // padding
+    gw = 700; // grid width
+    gh = 281; // grid height
+
+    c.beginPath();
+    for (var x = 0; x < gw; x+=43) {
+        c.moveTo(0.5 + x + p, p);
+        c.lineTo(0.5 + x + p, gh + p);
+    }
+    for (var y = 0; y < gh; y+=20) {
+        c.moveTo(p, 0.5 + y + p);
+        c.lineTo(gw + p, 0.5 + y + p);
+    }
+    c.strokeStyle = gridColor;
+    c.stroke();
+}
+
+
+function placeNote(c, notePlacement, noteValue, noteColor){
+    console.log(notePlacement)
+    console.log(noteValue)
+    var xpos = notePlacement; // change to calculated formula once you find one that works
+    var ypos = noteValue; // change to calculated formula once you find one that works
+    console.log("Xpos: " + xpos);
+    console.log("Ypos: " + ypos);
+    drawCircle(c, xpos, ypos, 7, noteColor)
+
+
+
+    /*
+    var yPosition = noteValue; // change to calculated formula once you find one that works
+    var xPosition = notePlacement; // change to calculated formula once you find one that works
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(100, yPosition, 4); // change this to xPosition when you figure it out
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+    //console.log("Note should be visible on screen.");
+    */
+}
+
+
+function drawCircle(ctx, x, y, radius, fill) {
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    if (fill) {
+      ctx.fillStyle = fill
+      ctx.fill()
+    }
+  }
+
+
+
+
 
 /* Discarded code:
 
