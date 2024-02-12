2659832aeeeddd484e58a4eeeff15297493e0060
Erin Murphy <erinmurphy843@gmail.com>
2024-02-12 11:47:01 -0500

    Add FAQ content, styling

--- frontend/src/app/components/accordion/accordion.component.ts
+++ frontend/src/app/components/accordion/accordion.component.ts
@@ -7,13 +7,23 @@ import { Component } from '@angular/core';
 export class AccordionComponent {
   faqs = [
     {
-      question: 'How to use this tool?',
-      answer: 'Instructions on using the tool...',
+      question: 'What does this tool do?',
+      answer:
+        'Welcome! The Music Box MIDI Visualizer converts MIDI (music note) data into a virtual paper strip for DIY music boxes. You can use the virtual paper strip as a reference when punching holes for your music box. The Visualizer was created as an extension of Music Box Maniacs, enabling users to punch notes in between the lines.',
     },
     {
-      question: 'How to upload MIDI files?',
-      answer: 'Steps to upload MIDI files...',
+      question: 'Who should use the Visualizer?',
+      answer: `Anyone who has a song with notes that need to be punched in between vertical lines. Some possible use cases are:
+        - A tune that has triplets.
+        - A tune that has 16th notes.
+        - A tune with a tempo of 80 BPM or higher. 
+        
+        With the Visualizer's 2x note spacing, you can condense a tune to use half the amount of paper and half the turning speed!
+        `,
     },
-    // Add more FAQs as needed
+    {
+      question: 'How can I use the Visualizer?',
+      answer: `1. Click "Select a MIDI File" and upload your .mid or .midi file.\n 2. Use the dropdowns to select your box type, desired spacing, and other settings.\n3. Voila! You now have a reference for your DIY music box. Happy punching!`,
+    },
   ];
 }
