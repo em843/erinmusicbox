a2c5d86e70a703625db0668dd3a7318b6a9973fb
Erin Murphy <erinmurphy843@gmail.com>
2024-02-19 16:35:58 -0500

    Description optional

--- frontend/src/app/interfaces/melody.interface.ts
+++ frontend/src/app/interfaces/melody.interface.ts
@@ -2,7 +2,7 @@ export interface Melody {
   title: string;
   artist?: string;
   url: string;
-  description: string;
+  description?: string;
   type: '15' | '20' | '30';
   created: Date;
   length: number;
