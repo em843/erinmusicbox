2efe233bb38aa86cf79ea712817d158d18b049d4
Erin Murphy <erinmurphy843@gmail.com>
2024-02-18 21:20:33 -0500

    Some responsiveness

--- frontend/src/app/pages/header/header.component.ts
+++ frontend/src/app/pages/header/header.component.ts
@@ -7,13 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  
   hasScrolled = false;
+  isMenuOpen = false;
   socialLinks = [
-    { name: 'YouTube', label: 'Watch', url: 'https://www.youtube.com/c/erinmusicbox/', icon: 'assets/youtube.svg'},
-    { name: 'Ko-Fi', label: 'Say Thanks', url: 'https://ko-fi.com/erinmusicbox', icon: 'assets/ko-fi.svg' },
-    { name: 'Fiverr', label: 'Commission', url: 'https://www.fiverr.com/erinmusicbox/arrange-any-song-for-your-diy-music-box-8343', icon: 'assets/fiverr.svg' },
-    { name: 'Music Box Maniacs', label: 'Listen', url: 'https://musicboxmaniacs.com/people/erinmusicbox/', icon: 'assets/mbm-logo.png' }
+    {
+      name: 'YouTube',
+      label: 'Watch',
+      url: 'https://www.youtube.com/c/erinmusicbox/',
+      icon: 'assets/youtube.svg',
+    },
+    {
+      name: 'Ko-Fi',
+      label: 'Say Thanks',
+      url: 'https://ko-fi.com/erinmusicbox',
+      icon: 'assets/ko-fi.svg',
+    },
+    {
+      name: 'Fiverr',
+      label: 'Commission',
+      url: 'https://www.fiverr.com/erinmusicbox/arrange-any-song-for-your-diy-music-box-8343',
+      icon: 'assets/fiverr.svg',
+    },
+    {
+      name: 'Music Box Maniacs',
+      label: 'Listen',
+      url: 'https://musicboxmaniacs.com/people/erinmusicbox/',
+      icon: 'assets/mbm-logo.png',
+    },
   ];
 
   ngOnInit(): void {
@@ -24,10 +44,14 @@ export class HeaderComponent {
     window.removeEventListener('scroll', this.onWindowScroll.bind(this), true);
   }
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   onWindowScroll() {
-    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log(scrollPosition)
+    const scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(scrollPosition);
     this.hasScrolled = scrollPosition > 0;
   }
-  
 }
