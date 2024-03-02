a13e504ff2af737a0804f508af87606b04888b06
Erin Murphy <erin.murphy@atomicobject.com>
2024-03-01 21:40:33 -0500

    Abstract about me card to component

--- frontend/src/app/components/component.module.ts
+++ frontend/src/app/components/component.module.ts
@@ -3,10 +3,11 @@ import { AccordionComponent } from './accordion/accordion.component';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MelodyCardComponent } from './melody-card/melody-card.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AccordionComponent, MelodyCardComponent],
+  declarations: [AccordionComponent, MelodyCardComponent, CardComponent],
   imports: [CommonModule, MatExpansionModule],
-  exports: [AccordionComponent, MelodyCardComponent],
+  exports: [AccordionComponent, MelodyCardComponent, CardComponent],
 })
 export class ComponentModule {}
