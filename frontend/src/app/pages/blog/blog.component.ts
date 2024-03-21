97878d10aa4b98f81f2f54c3ceb7c962dad6aae4
Erin Murphy <erinmurphy843@gmail.com>
2024-03-21 19:22:11 -0400

    Blog height bandaid fix

--- frontend/src/app/pages/blog/blog.component.ts
+++ frontend/src/app/pages/blog/blog.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'blog',
   templateUrl: './blog.component.html',
 })
 // @Injectable()
-export class BlogComponent {}
+export class BlogComponent {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: WindowEventMap['resize']) {
+    const windowWidth = (event.target as Window).innerWidth;
+    this.setIframeStyle(windowWidth);
+  }
+
+  iframeStyle = {
+    height: `calc(100vh - ' + 170 + 'px - ' + 24 + 'px)`,
+  };
+
+  setIframeStyle(windowWidth: number) {
+    if (windowWidth >= 640) {
+      // Tailwind's 'sm' breakpoint
+      this.iframeStyle = {
+        height: 'calc(100vh - ' + 170 + 'px - ' + 24 + 'px)', // For larger screens
+      };
+    } else {
+      this.iframeStyle = {
+        height: 'calc(100vh - ' + 140 + 'px - ' + 24 + 'px)', // For smaller screens
+      };
+    }
+  }
+}
